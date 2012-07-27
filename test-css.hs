25ef5fac52b18f28268f337537299e7b15ccf6bb
Sami Hangaslammi <sami.hangaslammi@leonidasoy.fi>
2012-07-27 13:34:35 +0300

    upgraded tests for hspec-1.3.0

--- test-css.hs
+++ test-css.hs
@@ -2,7 +2,7 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 import Test.HUnit
-import Test.Hspec
+import Test.Hspec (it, describe, hspec)
 import Test.Hspec.HUnit
 
 import Text.XML.HXT.Core
@@ -37,143 +37,139 @@ testProc fp sel op = do
 setText :: ArrowXml a => String -> a XmlTree XmlTree
 setText = replaceChildren . txt
 
-main = hspecX $ [describe "hxt-css-selectors" $
-    [ describe "quasi-quotes" $
-        [ it "should parse a name-selector" $ do
+main = hspec $ describe "hxt-css-selectors" $ do
+    describe "quasi-quotes" $ do
+        it "should parse a name-selector" $ do
             [css|body|] @?= SelectName "body"
 
-        , it "should parse a class-selector" $ do
+        it "should parse a class-selector" $ do
             [css|.first|] @?= SelectClass "first"
 
-        , it "should parse an id-selector" $ do
+        it "should parse an id-selector" $ do
             [css|#footer|] @?= SelectId "footer"
 
-        , it "should parse a combined name and class selector" $ do
+        it "should parse a combined name and class selector" $ do
             [css|p.first|] @?= Select [SelectName "p", SelectClass "first"]
 
-        , it "should parse a descendant selector" $ do
+        it "should parse a descendant selector" $ do
             [css|#footer a|] @?= SelectName "a" `DescendantOf` SelectId "footer"
 
-        , it "should parse direct child selection" $ do
+        it "should parse direct child selection" $ do
             [css|section > a|] @?= SelectName "a" `ChildOf` SelectName "section"
 
-        , it "should parse with correct precedence" $ do
+        it "should parse with correct precedence" $ do
             [css|a b > c d|] @?= SelectName "d" `DescendantOf` (SelectName "c" `ChildOf` (SelectName "b" `DescendantOf` SelectName "a"))
 
             [css|a > b, c d|] @?= SelectAll [SelectName "b" `ChildOf` SelectName "a", SelectName "d" `DescendantOf` SelectName "c"]
 
-        , it "should parse an any-selector" $ do
+        it "should parse an any-selector" $ do
             [css|a *|] @?= SelectAny `DescendantOf` SelectName "a"
 
-        , it "should parse sibling selector" $ do
+        it "should parse sibling selector" $ do
             [css|a ~ b|] @?= SelectName "b" `PrecededBy` SelectName "a"
 
-        , it "should parse adjancent selector" $ do
+        it "should parse adjancent selector" $ do
             [css|a + b|] @?= SelectName "b" `AdjacentTo` SelectName "a"
 
-        , it "should parse sibling selector in a descendant" $ do
+        it "should parse sibling selector in a descendant" $ do
             [css|a b ~ c|] @?= (SelectName "c" `PrecededBy` SelectName "b") `DescendantOf` SelectName "a"
-        ]
 
-    , describe "selecting" $
-        [ it "should select by element name" $ do
+    describe "selecting" $ do
+        it "should select by element name" $ do
             res <- testSel [css|body|] getName
             res @?= ["body"]
 
-        , it "should select by class name" $ do
+        it "should select by class name" $ do
             res <- testSel [css|.first|] getName
             res @?= ["a", "p"]
 
-        , it "should select by id" $ do
+        it "should select by id" $ do
             res <- testSel [css|#footer|] (getName &&& getAttrValue "class")
             res @?= [("section", "bottom")]
 
-        , it "should select by both name and class" $ do
+        it "should select by both name and class" $ do
             res <- testSel [css|p.first|] (getName &&& getAttrValue "class")
             res @?= [("p", "first")]
 
-        , it "should select direct descendants" $ do
+        it "should select direct descendants" $ do
             res <- testSel [css|#footer a|] (getName &&& getAttrValue "class")
             res @?= [("a", "contact")]
 
-        , it "should select all descendants" $ do
+        it "should select all descendants" $ do
             res <- testSel [css|body a|] (getName &&& getAttrValue "class")
             res @?= [("a", "first"), ("a", "second"),("a", "contact")]
 
-        , it "should only select direct children" $ do
+        it "should only select direct children" $ do
             res <- testSel [css|section > a|] (getName &&& getAttrValue "class")
             res @?= [("a", "contact")]
 
-        , it "should select many patterns" $ do
+        it "should select many patterns" $ do
             res <- testSel [css|#header, #content p|] (getName &&& getAttrValue "class")
             res @?= [("section" ,"top"),("p" ,"first"), ("p", "second")]
 
-        , it "should match anything with *" $ do
+        it "should match anything with *" $ do
             res <- testSel [css|body > *|] (getName &&& getAttrValue "class")
             res @?= [("section","top"),("section","middle"),("section","bottom")]
 
-        , it "should match preceding selector" $ do
+        it "should match preceding selector" $ do
             res <- testSel [css|li.one ~ li|] (getName &&& getAttrValue "class")
             res @?= [("li","two"),("li","three")]
 
-        , it "should match adjancent selector" $ do
+        it "should match adjancent selector" $ do
             res <- testSel [css|li.one + li|] (getName &&& getAttrValue "class")
             res @?= [("li","two")]
 
-        , it "shoud match preceding selector in a descendant" $ do
+        it "shoud match preceding selector in a descendant" $ do
             res <- testSel [css|#content li.one ~ li|] (getName &&& getAttrValue "class")
             res @?= [("li","two"),("li","three")]
 
-        , it "shoud match preceding selector in a direct descendant" $ do
+        it "shoud match preceding selector in a direct descendant" $ do
             res <- testSel [css|ul li.one ~ li|] (getName &&& getAttrValue "class")
             res @?= [("li","two"),("li","three")]
 
-        , it "shoud match preceding selector in a child" $ do
+        it "shoud match preceding selector in a child" $ do
             res <- testSel [css|ul > li.one ~ li|] (getName &&& getAttrValue "class")
             res @?= [("li","two"),("li","three")]
 
-        , it "should match adjancent selector in a descendant" $ do
+        it "should match adjancent selector in a descendant" $ do
             res <- testSel [css|#content li.one + li|] (getName &&& getAttrValue "class")
             res @?= [("li","two")]
 
-        , it "should match adjancent selector in a direct descendant" $ do
+        it "should match adjancent selector in a direct descendant" $ do
             res <- testSel [css|ul li.one + li|] (getName &&& getAttrValue "class")
             res @?= [("li","two")]
 
-        , it "should match adjancent selector in a child" $ do
+        it "should match adjancent selector in a child" $ do
             res <- testSel [css|ul > li.one + li|] (getName &&& getAttrValue "class")
             res @?= [("li","two")]
 
-        , it "should match descendants in sibling nodes" $ do
+        it "should match descendants in sibling nodes" $ do
             res <- testSel [css|p.first ~ ul li|] (getName &&& getAttrValue "class")
             res @?= [("li","one"),("li","two"),("li","three")]
 
-        , it "should match elements that contain an attribute" $ do
+        it "should match elements that contain an attribute" $ do
             res <- testSel [css|*[id]|] (getName &&& getAttrValue "id")
             res @?= [("section","header"),("section","content"),("section","footer")]
 
-        , it "should match elements that contain a specific attribute value" $ do
+        it "should match elements that contain a specific attribute value" $ do
             res <- testSel [css|*[class="first"]|] (getName &&& getAttrValue "class")
             res @?= [("a","first"),("p","first")]
-        ]
 
-    , describe "processing" $
-        [ it "should apply changes to a named node" $ do
+    describe "processing" $ do
+        it "should apply changes to a named node" $ do
             testProc "test-title-change" [css|title|] (setText "Foobar")
 
-        , it "should apply changes to all nodes with a class" $ do
+        it "should apply changes to all nodes with a class" $ do
             testProc "test-class-proc" [css|.first|] (setText "Edited")
 
-        , it "should apply changes to all nodes with specific name and class" $ do
+        it "should apply changes to all nodes with specific name and class" $ do
             testProc "test-multi-assign" [css|li.one, li.three|] (setText "Edited")
 
-        , it "should apply changes with descendant selector" $ do
+        it "should apply changes with descendant selector" $ do
             testProc "test-descendant-assign" [css|#header a|] (setText "Edited")
 
-        , it "should apply changes with a child selector" $ do
+        it "should apply changes with a child selector" $ do
             testProc "test-child-assign" [css|section > a|] (setText "Edited")
 
-        , it "should apply changes with a sibling selector" $ do
+        it "should apply changes with a sibling selector" $ do
             testProc "test-sibling-assign" [css|li.one ~ *|] (setText "Edited")
-        ]
-    ]]
